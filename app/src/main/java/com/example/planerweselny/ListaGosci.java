293d358ddf070165d80ffa842a58e4c1d3530537
klemawa <116188369+klemawa@users.noreply.github.com>
2023-06-09 15:18:10 +0200

    nowe nowe 9.06

--- app/src/main/java/com/example/planerweselny/ListaGosci.java
+++ app/src/main/java/com/example/planerweselny/ListaGosci.java
@@ -2,7 +2,9 @@ package com.example.planerweselny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListaGosci extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class ListaGosci extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_gosci);
+    }
+    public void przejscieOdliczaj(View v){ //przejście przyciskiem do okna home
+        Intent i = new Intent(this,HomeActivity.class);
+        startActivity(i);
+    }
+    public void przejscieSzukaj(View v){ //przejście przyciskiem do okna szukaj
+        Intent i = new Intent(this,SzukajActivity.class);
+        startActivity(i);
+    }
+    public void przejscieKalendarz(View v){  ////przejście przyciskiem do okna kalendarz
+        Intent i = new Intent(this,KalendarzActivity.class);
+        startActivity(i);
+    }
+    public void przejsciePlanuj(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,PlanujActivity.class);
+        startActivity(i);
+    }
+    public void przejscieKonto(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,KontoActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/planerweselny/PlanujActivity.java
+++ app/src/main/java/com/example/planerweselny/PlanujActivity.java
@@ -35,8 +35,26 @@ public class PlanujActivity extends AppCompatActivity {
         Intent i = new Intent(this,Ankieta.class);
         startActivity(i);
     }
-    public void przejscieBudzet(View v){ //przejście przyciskiem do okna konto
-        Intent i = new Intent(this,Budzet.class);
+
+    public void przejscieListaGosci(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,ListaGosci.class);
         startActivity(i);
     }
+    public void przejsciePlanStolow(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,PlanStolow.class);
+        startActivity(i);
+    }
+    public void przejscieListaZadan(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,ListaZadan.class);
+        startActivity(i);
+    }
+    public void przejscieQA(View v){ //przejście przyciskiem do okna konto
+        Intent i = new Intent(this,QA.class);
+        startActivity(i);
+    }
+    public void przejscieBudzet(View v) { //przejście przyciskiem do okna konto
+        Intent i = new Intent(this, Budzet.class);
+        startActivity(i);
+    }
+
 }
